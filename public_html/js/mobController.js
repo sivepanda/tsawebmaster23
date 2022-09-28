0815a91e9218e217fef64af1f978b9cfda1926cd
sivepanda <sivenpanda@icloud.com>
2022-09-27 21:52:44 -0500

    back to square one

---
+++ public_html/js/mobController.js
@@ -0,0 +1,59 @@
+//HI AHMAD. CHANGE THIS AND **ONLY** THIS :)
+newMobile = false; // <-THIS GUY. SEE IT? False for old mobile, true for new
+//YEP. THAT'S IT. STOP HERE
+
+var height = window.innerHeight;
+var width = window.innerWidth;
+var isOpen = false;
+var dtstyles = 'styles/desktopstyles.css';
+var mobstyles = newMobile ? 'styles/experimentalmobile.css' : 'styles/mobilestyles.css';
+var styles = document.createElement('link');
+var isMobile;
+var initialLogoHeight;
+var initialTextIBP3Height;
+
+styles.setAttribute('rel', 'stylesheet');
+
+resizeWindow();
+document.head.appendChild(styles);
+const head = document.getElementById("header");
+
+function changeElementIDDisplay(id, display) {
+    document.getElementById(id) ? document.getElementById(id).style.display = display : console.log(id + " replaced");
+}
+
+// var logo = document.querySelector('.menumenu');
+var menu = document.querySelector('.menu');
+
+// logo.addEventListener('click', function() {
+//     menu.classList.toggle('showmenu');
+// });
+
+function resizeWindow() {
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    console.log("window size ratio: " + width / height);
+
+    if ((width / height) > (7 / 6)) {
+        isMobile = false;
+        styles.setAttribute('href', dtstyles);
+    } else {
+        isMobile = true;
+        styles.setAttribute('href', mobstyles);
+        window.scrollTo(0, 0);
+
+    }
+}
+
+
+
+function toggleMenu() {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+    mobile_list.classList.toggle('is-active');
+    console.log('menu loaded');
+
+}
+
+
+window.addEventListener("resize", resizeWindow);
