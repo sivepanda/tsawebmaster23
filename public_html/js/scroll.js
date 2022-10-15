f409f13cecdb2e772a6590d50aa2814d5df3b7eb
sivepanda <sivenpanda@icloud.com>
2022-10-15 00:58:42 -0500

    horiz scroll

---
+++ public_html/js/scroll.js
@@ -0,0 +1,33 @@
+function elementInViewport(el) {
+    var top = el.offsetTop;
+    var left = el.offsetLeft;
+    var width = el.offsetWidth;
+    var height = el.offsetHeight;
+
+    while (el.offsetParent) {
+        el = el.offsetParent;
+        top += el.offsetTop;
+        left += el.offsetLeft;
+    }
+
+    return (
+        top >= window.pageYOffset &&
+        left >= window.pageXOffset &&
+        (top + height) <= (window.pageYOffset + window.innerHeight) &&
+        (left + width) <= (window.pageXOffset + window.innerWidth)
+    );
+}
+
+
+const scrollContainer = document.getElementsByClassName("horiz")[0];
+
+scrollContainer.scrollLeft = 5;
+
+window.addEventListener("scroll", function(e) {
+    if (elementInViewport(scrollContainer)) {
+        scrollContainer.addEventListener("wheel", (evt) => {
+            evt.preventDefault();
+            scrollContainer.scrollLeft += evt.deltaY;
+        });
+    }
+});
