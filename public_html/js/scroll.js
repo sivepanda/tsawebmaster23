f8d5a6ea4a3e3a72251d1cc3a5569319bd5ef1e6
sivepanda <sivenpanda@icloud.com>
2022-10-19 18:37:40 -0500

    side scrol

--- public_html/js/scroll.js
+++ public_html/js/scroll.js
@@ -20,14 +20,27 @@ function elementInViewport(el) {
 
 
 const scrollContainer = document.getElementsByClassName("horiz")[0];
+const scrollElements = document.getElementsByClassName("helement");
+var dists = []
+for (el in scrollElements) {
+    dists.push(el.offsetLeft)
+}
 
-scrollContainer.scrollLeft = 5;
+//HERE LIES BROKEN SCROLL CODE
 
-window.addEventListener("scroll", function(e) {
-    if (elementInViewport(scrollContainer)) {
-        scrollContainer.addEventListener("wheel", (evt) => {
-            evt.preventDefault();
-            scrollContainer.scrollLeft += .5 * (evt.deltaY);
-        });
-    }
-});
+// scrollContainer.scrollLeft = .5;
+// var scr = 0;
+
+// window.addEventListener("scroll", function(e) {
+//     scrollContainer.addEventListener("wheel", (evt) => {
+//         evt.preventDefault();
+//         // if (evt.deltaY > 0 && scr < dists.length) {
+//         //     scr++;
+//         //     scrollContainer.scrollLeft = dists[scr];
+//         // } else if (evt.deltaY < 0 && scr > 0) {
+//         //     scr--;
+//         //     scrollContainer.scrollLeft = dists[scr];
+//         // }
+//         // scrollContainer.scrollLeft += evt.deltaY * 10;
+//     });
+// });
