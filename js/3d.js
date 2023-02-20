edd171bf5616d1ee5548b2b2d0a8a24e1a4d8475
Hyperion3224 <shrimpman911@gmail.com>
2023-02-19 19:33:43 -0600

    lkjhgfdsa

--- js/3d.js
+++ js/3d.js
@@ -1,4 +1,4 @@
-import { GLTFLoader } from 'js/libraries/GLTFLoader.js';
+import { GLTFLoader } from '../js/libraries/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
