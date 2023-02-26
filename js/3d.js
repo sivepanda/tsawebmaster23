05334644d5fba4c3a95c9e400cfa7548c321efba
sivepanda <sivenpanda@icloud.com>
2023-02-26 16:55:06 -0600

    ini 3d

--- js/3d.js
+++ js/3d.js
@@ -1,38 +1,72 @@
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+
+// import './styles.'
 
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 2000 );
 
-const renderer = new THREE.WebGLRenderer();
+scene.background = new THREE.Color( 0x000000 );
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+camera.position.setX(90);
+camera.position.setY(0);
+camera.position.setZ(10);
+camera.rotation.x = -0.5 * Math.PI
 
-camera.position.z = 40;
 
-const loader = new GLTFLoader();
+renderer.render( scene, camera );
 
-loader.load( '../resources/3d/SavitirII.glb', function ( gltf ) {
 
-	scene.add( gltf.scene );
-	
-}, undefined, function ( error ) {
+// instantiate a loader
+const loader = new OBJLoader();
 
-	console.error( error );
+const mtlLoader = new MTLLoader();
 
-} );
+const base = '../resources/3d/'
+
+mtlLoader.load((base + 'SavitirII_gr.mtl'), function(materials) {	
+	materials.preload();
+
+	loader.setMaterials(materials);
+
+	loader.load((base + 'SavitirII_gr.obj'), function(object){
+		scene.add(object);
+		object.rotation.y = 0.75 * Math.PI
+	});
+});
+
+const pointLight = new THREE.PointLight(0xffffff);
+// pointLight.position.set(0, 25, 25);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+scene.add(lightHelper)
+
+
 
 function animate() {
 	requestAnimationFrame( animate );
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 
 animate();
+
+
+
+
+
+
+
