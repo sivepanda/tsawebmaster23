1b4660477175b65e544d8ad8931a2cdb8c2ec2a6
Siven Panda <sivenpanda@icloud.com>
2023-02-28 01:39:54 -0600

    attempt new rocket model for time being

--- js/3d.js
+++ js/3d.js
@@ -9,7 +9,7 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-scene.background = new THREE.Color( 0xff0000 );
+scene.background = new THREE.Color( 0x000000 );
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
