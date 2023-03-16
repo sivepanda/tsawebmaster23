05104afbe74b496fa797b87567bc4fb3e872dde0
sivepanda <sivenpanda@icloud.com>
2023-03-16 03:59:05 -0500

    3d stuff

--- js/3d.js
+++ js/3d.js
@@ -1,8 +1,8 @@
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+// import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+// import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
+// import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
 // import { CSS3DObject } from 'three/addons/renderers/CSS3DObject.js';
 
 // animating POSES https://dev.to/pahund/animating-camera-movement-in-three-js-17e9
@@ -10,7 +10,6 @@ import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer
 
 import * as THREE from 'three';
 
-import TWEEN from 'tween';
 
 const scene = new THREE.Scene();
 
@@ -22,30 +21,17 @@ const renderer = new THREE.WebGLRenderer({
 	canvas: document.querySelector("#bg"),
 });
 
-const cssrenderer = new CSS3DRenderer();
-cssrenderer.setSize(window.innerWidth, window.innerHeight);
-cssrenderer.domElement.style.position = 'absolute';
-cssrenderer.domElement.style.top = '0px';
-cssrenderer.domElement.style.pointerEvents = 'none';
-document.body.appendChild(cssrenderer.domElement);
-
-const div1 = document.createElement('div');
-var div1Content = "<h1>The Engines</h1> <p>The engines are pretty cool. They do some really cool things, like taking the astrotronauts to space without blowing them up.</p>"
-div1.innerHTML += div1Content;
-
-
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.outputEncoding = THREE.sRGBEncoding;
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 // camera.position.set(0, 10, 10);
-controls.update();
+// controls.update();
 // camera.rotation.z = -0.5 * Math.PI
 
-const initialPos = { pX: 0.037, pY: -4.475, pZ: 2.386, 
-	rX: 1.068, rY: -0.017, rZ: 0.030 }
+const initialPos = { pX: -3.045818102784073, pY: 11.708038654057017, pZ: 9.13024895854543, rX: 0.12247009903293854, rY: -0.26363018841530583, rZ: 0.03206364892180843 }
 camera.position.set(initialPos.pX, initialPos.pY, initialPos.pZ);
 camera.rotation.set(initialPos.rX, initialPos.rY, initialPos.rZ);
 
@@ -71,6 +57,10 @@ loader.load(
 	// called while loading is progressing
 	function ( xhr ) {
 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		if((xhr.loaded / xhr.total * 100) == 100) {
+			isLoaded = true;
+    		loaded();
+		}
 	},
 	// called when loading has errors
 	function ( error ) {
@@ -79,6 +69,27 @@ loader.load(
 
 	}
 )
+
+/*LOADING SCREEN */
+/**what happens when the page loads*/
+function loaded() {
+    if (isLoaded && timerComplete && document.getElementById("loadtank")) {
+        var load = document.getElementById("loadtank");
+        load.remove();
+        document.getElementById("ic0") ? document.querySelector("#ic0 h1").classList.add("showh1") : console.log('ic0 not found');
+        document.getElementById("ic0") ? document.querySelector("#ic0 p").classList.add("showp") : console.log('ic0 not found');
+        
+        // REENABLE BELOW
+        document.getElementById("")
+        window.clearTimeout(loadInterval);
+        console.log('Page loaded successfully');
+    }
+}
+var loadInterval; 
+loadInterval = setInterval(function() {
+    timerComplete = true;
+    loaded();
+}, 1500);
 
 const pointLight = new THREE.PointLight(0xffffff);
 
@@ -89,13 +100,6 @@ scene.add(pointLight, ambientLight)
 const lightHelper = new THREE.PointLightHelper(pointLight)
 // scene.add(lightHelper);
 
-div1.style.backgroundColor = "green";
-
-var cssElementOne = new CSS3DObject(div1);
-cssElementOne.position.set(5, 5, 5);
-
-scene.add(cssElementOne);
-
 
 /**This function is to set the different setpoints around the rocket that can be triggered */
 function setCameraSetpoint(setpoint) {
@@ -103,8 +107,7 @@ function setCameraSetpoint(setpoint) {
 		rX: camera.rotation.x, rY: camera.rotation.y, rZ: camera.rotation.z };
 	switch(setpoint) {
 		case 1: // full
-			const beginPosCoords = { pX: 0.037, pY: -4.475, pZ: 2.386, 
-				rX: 1.068, rY: -0.017, rZ: 0.030 }
+			const beginPosCoords = initialPos
 				new TWEEN.Tween(cameraCoords)
 				.to(beginPosCoords)
 				.onUpdate(() => {
@@ -114,8 +117,7 @@ function setCameraSetpoint(setpoint) {
 				.start();
 			break;
 		case 2: // engines
-			const engPosCoords = { pX: 4.844, pY: 1.209, pZ: 3.953, 
-				rX: -0.001, rY: 0.794, rZ: 0 };
+			const engPosCoords = { pX: -1.6123642556361932, pY: 8.429628340056222, pZ: 4.389562028934395, rX: 0.09014302180271692, rY: -0.2451981612784777, rZ: 0.021938031811923497 };
 			new TWEEN.Tween(cameraCoords)
 				.to(engPosCoords)
 				.onUpdate(() => {
@@ -125,8 +127,7 @@ function setCameraSetpoint(setpoint) {
 				.start();
 			break;
 		case 3: //midstage
-			const midPosCoords = { pX: 4.041051621581688, pY: 3.7530026023575385, pZ: 0.9745105259344332, 
-					rX: -1.066, rY: 1.077, rZ: 1.01 };
+			const midPosCoords = { pX: 1.321518117638575, pY: 2.0845178566371403, pZ: 5.021915127139023, rX: 0.019034491878618835, rY: 0.4264218409609203, rZ: -0.007873753243657721 };
 				new TWEEN.Tween(cameraCoords)
 				.to(midPosCoords)
 				.onUpdate(() => {
@@ -136,7 +137,7 @@ function setCameraSetpoint(setpoint) {
 				.start();
 			break;
 		case 4: //midstage
-			const wholePosCoords = { pX: -1.7889429186971835, pY: 3.2115692238692475, pZ: 13.61988483323175, rX: 0.6415454260078526, rY: -0.1440605609522377, rZ: 0.10682581834083991 };
+			const wholePosCoords = { pX: -0.03303492140215647, pY: -4.286697265748935, pZ: 2.8663965642543348, rX: 0.9626726061176113, rY: 0.10014884938174967, rZ: -0.1426493235351751 };
 				new TWEEN.Tween(cameraCoords)
 				.to(wholePosCoords)
 				.onUpdate(() => {
@@ -152,7 +153,6 @@ function animate() {
 	requestAnimationFrame( animate );
 	// controls.update();
 	renderer.render( scene, camera );
-	cssrenderer.render( scene, camera );
 	TWEEN.update();
 }
 
@@ -160,19 +160,33 @@ window.addEventListener('resize', () => {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	cssrenderer.setSize(window.innerWidth, window.innerHeight);
 })
 
 animate();
 
 var nSet = 1;
 
-// addEventListener("wheel", (event) => {
-// 	console.log(nSet)
-// 	setCameraSetpoint(nSet);
-// 	nSet++;
-// 	nSet = (nSet > 4) ? 1 : nSet;
-// });
+addEventListener("wheel", (event) => {
+	console.log(nSet)
+	if(event.deltaY > 0) {
+		nSet++;
+		if(nSet > 4) {
+			nSet = 1;
+			document.getElementById("infopanels").scrollTop = 0;
+
+		} else {
+			document.getElementById("infopanels").scrollBy(0, 5);
+		}
+		// nSet = (nSet > 4) ? 1 : nSet;
+		setCameraSetpoint(nSet);
+	} else {
+		setCameraSetpoint(nSet);
+		nSet--;
+		nSet = (nSet < 1) ? 4 : nSet;
+		document.getElementById("infopanels").scrollBy(0, -5);
+	}
+	
+});
 
 document.onkeydown = (e) => {
 	if(e.key  == 'ArrowLeft') {
@@ -183,6 +197,16 @@ document.onkeydown = (e) => {
 		setCameraSetpoint(nSet);
 		nSet++;
 		nSet = (nSet > 4) ? 1 : nSet;
+	}
+	else if(e.key == 's') {
+		// Data which will write in a file.
+		let posData = "{  X: " + camera.position.x + ", Y: " + camera.position.y +  ", Z: " + camera.position.z + "  }"
+		let rotData = "{  X: " + camera.rotation.x + ", Y: " + camera.rotation.y +  ", Z: " + camera.rotation.z + "  }"
+		let data = "{ pX: " + camera.position.x + ", pY: " + camera.position.y + ", pZ: " + camera.position.z + ", rX: " + camera.rotation.x + ", rY: " + camera.rotation.y + ", rZ: " + camera.rotation.z + " }"
+		
+		console.log("Position", posData)
+		console.log("Rotation", rotData)
+		console.log("Data", data)
 	}
 }
 
