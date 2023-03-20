3ae2b7d9819eaf628254f98004b340689f613ffd
sivepanda <sivenpanda@icloud.com>
2023-03-19 21:44:34 -0500

    revive purchase

--- js/3d.js
+++ js/3d.js
@@ -13,7 +13,7 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-scene.background = new THREE.Color( 0x10141f );
+scene.background = new THREE.Color( 0x181818 );
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 console.log("test")
